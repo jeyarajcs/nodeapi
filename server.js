1c6fd220022b2e523d286cdf9b7708b5060bc85c
unknown <jeyraj.1994@gmail.com>
2016-12-26 00:31:43 +0530

    exprerss tested with json data

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res){
+app.get('/test', function(req,res){
 	
-	res.send('Hello API');
+	//res.send('Hello ');
+	res.json({status:'success',message:[{name:'jeyaraj',age:22,company:'zozz'},{name:'Shiva',age:24,company:'google'}]});
 })
 
 app.listen(3030, function(){
